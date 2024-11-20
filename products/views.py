2077b58dc805f97459f67bb17bb497dac0f33a33
Parsa <pheydari33@yahoo.com>
2024-11-20 22:23:50 +0330

    create productlist view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Category, Product, File
+from .serializers import CategorySerializer, FileSerializer, ProductSerializer
+
+
+
+class ProductListView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True, context={'request': request})
+        return Response(serializer.data)
