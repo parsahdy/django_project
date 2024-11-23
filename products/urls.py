7e59dbbcfbffff566e82e1ad0b4f8bd6d3bc337d
Parsa <pheydari33@yahoo.com>
2024-11-23 21:04:30 +0330

    add actegories and files urls

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path('products/', views.ProductListView.as_view()),
-]
+    path('products/', views.ProductListView.as_view(), name='product-list'),
+    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
+    path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
+
+    path('products/<int:product_id>/files/', views.FileListView.as_view(), name='file-list'),
+    path('products/<int:product_id>/files/<int:pk>/', views.FileDetailView.as_view(), name='file-detail'),
+
+
+] 
