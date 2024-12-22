4b5f654077d1f2770eaa344f7af1e3b62899e46c
Parsa <pheydari33@yahoo.com>
2024-12-22 13:40:44 +0330

    add serializer

---
+++ subscriptions/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from .models import Package, Subscription
+
+
+
+class PackageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Package
+        fields = ('title', 'sku', 'description', 'avatar', 'price', 'duration')
+
+
+
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    Package = PackageSerializer()
+
+    class Meta:
+        model = Subscription
+        fields = ('package', 'created_time', 'expire_time')
